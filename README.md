0db78a61e16a5a1588cefbbde2d7f42e6dc94173
Tomas Orti <tomas.orti.martin@ericsson.com>
2022-02-17 01:18:00 +0100

    Added files from sfml-games project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 4asteroids
+
+```
+cd SFML
+git fetch && git checkout 2.5.x
+git branch
+```
---
+++ build.bat
@@ -0,0 +1,1 @@
+cl.exe /nologo /std:c++14 /EHsc /ISFML\include main.cpp /Zi /Od /D WIN32 /D _WINDOWS /MDd /link /DEBUG /PDB:"D:/playground/c_cpp/4asteroids/4asteroids.pdb" /machine:x64 /libpath:SFML\build\lib\Debug sfml-system-d.lib sfml-window-d.lib sfml-graphics-d.lib sfml-audio-d.lib /out:4asteroids.exe
